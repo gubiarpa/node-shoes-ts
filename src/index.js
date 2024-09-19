ff128066d28885b6e51872612085faf4eb0ddbda
Billy Arredondo <billyarredondo.coder@gmail.com>
2024-09-19 01:57:42 -0500

    Get Product by ID

--- src/index.js
+++ src/index.js
@@ -65,6 +65,20 @@ app.get('/api/products', async (_, res) => {
 	}
 })
 
+app.get('/api/products/:id', async (req, res) => {
+	try {
+		const { id } = req.params
+		const product = await Product.findById(id)
+		if (!product) {
+			return res.status(404).json({ message: 'Product not found' })
+		}
+		return res.status(200).json(product)
+	} catch (error) {
+		console.error(error)
+		res.status(500).json({ error: 'Error when retrieving products' })
+	}
+})
+
 // Iniciar el servidor
 app.listen(port, () => {
 	console.log(`Servidor escuchando en http://localhost:${port}`)
