fa8453b0d09bafe3c182e73fb2f7e85ef21114c4
Billy Arredondo <billyarredondo.coder@gmail.com>
2024-10-03 00:30:04 -0500

    Create Sell Validation (#1)

--- src/models/Sell.js
+++ src/models/Sell.js
@@ -1,11 +1,17 @@
 const mongoose = require('mongoose')
 
-const sellSchema = new mongoose.Schema({
-	productId: String,
-	quantity: Number,
-	amount: Number,
-	paymentMethodId: String,
-})
+const sellSchema = new mongoose.Schema(
+	{
+		productId: String,
+		quantity: Number,
+		amount: Number,
+		paymentMethodId: String,
+	},
+	{
+		versionKey: false,
+		timestamps: true,
+	}
+)
 
 const Sell = mongoose.model('Sell', sellSchema)
 
